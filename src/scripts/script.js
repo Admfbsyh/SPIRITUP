347d53a9de42d5feb7748d3b7b589a638011475f
Admfbsyh <72254331+Admfbsyh@users.noreply.github.com>
2022-12-02 16:29:10 +0700

    Update script.js
    Update upload avatar image

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,36 @@
+// Add Menu Hamburger
 const menu = document.getElementById("menu-label");
 const sidebar = document.getElementsByClassName("sidebar")[0];
+const imgDiv = document.querySelector(".list-item-user");
+const img = document.querySelector("#photo");
+const file = document.querySelector("#file");
+const uploadBtn = document.querySelector("#uploadBtn");
 
 menu.addEventListener("click", function () {
   sidebar.classList.toggle("hide");
   console.log("ok");
+});
+
+// Add Choosed File User Profile
+
+imgDiv.addEventListener("mouseenter", function () {
+  uploadBtn.style.display = "block";
+});
+
+imgDiv.addEventListener("mouseleave", function () {
+  uploadBtn.style.display = "none";
+});
+
+file.addEventListener("change", function () {
+  const choosedFile = this.files[0];
+
+  if (choosedFile) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+      img.setAttribute("src", reader.result);
+    });
+
+    reader.readAsDataURL(choosedFile);
+  }
 });
