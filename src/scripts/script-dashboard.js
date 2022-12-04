c701cd4bf83cb3d1b2c4564fd339e560f2830b49
Admfbsyh <72254331+Admfbsyh@users.noreply.github.com>
2022-12-04 09:38:06 +0700

    update dashboard.js
    revisi dashboard

--- src/scripts/script-dashboard.js
+++ src/scripts/script-dashboard.js
@@ -1,208 +1,264 @@
 const incompleteCreatetaskfList = [];
-const RENDER_EVENT = 'render-task';
-const SAVED_EVENT = 'saved-task';
-const STORAGE_KEY = 'SPIRITUP_APPS';
+const RENDER_EVENT = "render-task";
+const SAVED_EVENT = "saved-task";
+const STORAGE_KEY = "SPIRITUP_APPS";
 
 function generateId() {
-    return +new Date();
+  return +new Date();
 }
 
 function generateTaskObject(id, title, date, description, isCompleted) {
-    return {
-      id,
-      title,
-      date,
-      description,
-      isCompleted
-    }
+  return {
+    id,
+    title,
+    date,
+    description,
+    isCompleted,
+  };
 }
 
 function findTask(titleId) {
-    for (const taskItem of incompleteCreatetaskfList) {
-      if (taskItem.id === titleId) {
-        return taskItem;
-      }
+  for (const taskItem of incompleteCreatetaskfList) {
+    if (taskItem.id === titleId) {
+      return taskItem;
     }
-    return null;
+  }
+  return null;
 }
 
 function findTaskIndex(titleId) {
-    for (index in incompleteCreatetaskfList) {
-      if (incompleteCreatetaskfList[index].id === titleId) {
-        return index;
-      }
+  for (index in incompleteCreatetaskfList) {
+    if (incompleteCreatetaskfList[index].id === titleId) {
+      return index;
     }
-    return -1;
+  }
+  return -1;
 }
 
 function isStorageExist() {
-    if (typeof (Storage) === undefined) {
-      alert('Browser kamu tidak mendukung local storage');
-      return false;
-    }
-    return true;
+  if (typeof Storage === undefined) {
+    alert("Browser kamu tidak mendukung local storage");
+    return false;
+  }
+  return true;
 }
 
 function saveData() {
-    if (isStorageExist()) {
-      const parsed = JSON.stringify(incompleteCreatetaskfList);
-      localStorage.setItem(STORAGE_KEY, parsed);
-      document.dispatchEvent(new Event(SAVED_EVENT));
-    }
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(incompleteCreatetaskfList);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
 }
 
 function loadDataFromStorage() {
-    const serializedData = localStorage.getItem(STORAGE_KEY);
-    let data = JSON.parse(serializedData);
-  
-    if (data !== null) {
-      for (const task of data) {
-        incompleteCreatetaskfList.push(task);
-      }
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const task of data) {
+      incompleteCreatetaskfList.push(task);
     }
-  
-    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
 function makeTask(taskObject) {
-    const textTask = document.createElement('h2');
-    textTask.innerText = taskObject.title;
-   
-    const textDate = document.createElement('p');
-    textDate.innerText = taskObject.date;
+  const textTask = document.createElement("h2");
+  textTask.innerText = taskObject.title;
 
-    const textDescription = document.createElement('p');
-    textDescription.innerText = taskObject.description;
-   
-    const textnewTask = document.createElement('div');
-    textnewTask.classList.add('task_item');
-    textnewTask.append(textTask, textDate, textDescription);
-   
-    const task_shelf = document.createElement('div');
-    task_shelf.classList.add('item', 'inner');
-   
-    task_shelf.append(textnewTask);
-    task_shelf.setAttribute('id', `task-${taskObject.id}`);
+  const textDate = document.createElement("p");
+  textDate.innerText = taskObject.date;
 
-    if (taskObject.isCompleted) {
-        const undoButton = document.createElement('button');
-        undoButton.classList.add('undo-button');
-    
-        undoButton.addEventListener('click', function () {
-        undoTaskFromCompleted(taskObject.id);
-        });
-    
-        const trashButton = document.createElement('button');
-        trashButton.classList.add('trash-button');
-    
-        trashButton.addEventListener('click', function () {
-        removeTaskFromCompleted(taskObject.id);
-        });
-    
-        task_shelf.append(undoButton, trashButton);
-    } else {
-        const checkButton = document.createElement('button');
-        checkButton.classList.add('check-button');
-        
-        checkButton.addEventListener('click', function () {
-        addTaskToCompleted(taskObject.id);
-        });
+  const textDescription = document.createElement("p");
+  textDescription.innerText = taskObject.description;
 
-        const trashButton = document.createElement('button');
-        trashButton.classList.add('trash-button');
-    
-        trashButton.addEventListener('click', function () {
-        removeTaskFromCompleted(taskObject.id);
-        });
-        
-        task_shelf.append(checkButton, trashButton);
-    }
+  const textnewTask = document.createElement("div");
+  textnewTask.classList.add("task_item");
+  textnewTask.append(textTask, textDate, textDescription);
 
-    return task_shelf;
+  const task_shelf = document.createElement("div");
+  task_shelf.classList.add("item", "inner");
 
+  task_shelf.append(textnewTask);
+  task_shelf.setAttribute("id", `task-${taskObject.id}`);
+
+  if (taskObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+
+    undoButton.addEventListener("click", function () {
+      alert("Apakah anda yakin ingin mengembalikan task ke Regular task?");
+      undoTaskFromCompleted(taskObject.id);
+    });
+
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      alert(
+        "Selamat, silahkan isi form bukti kegiatan untuk menuju ke completed"
+      );
+      addTaskToCompleted(taskObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(taskObject.id);
+    });
+
+    task_shelf.append(trashButton, undoButton, checkButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(taskObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(taskObject.id);
+    });
+
+    const replaceButton = document.createElement("button");
+    replaceButton.classList.add("replace-button");
+
+    replaceButton.addEventListener("click", function () {
+      alert(
+        "Tugas anda akan dialihkan pada daftar task Important. Tekan OK untuk melanjutkan"
+      );
+      replaceTaskFromCompleted(taskObject.id);
+    });
+
+    task_shelf.append(trashButton, replaceButton, checkButton);
+  }
+
+  return task_shelf;
 }
 
 function addTask() {
-    const inputTask = document.getElementById('inputTaskTitle').value;
-    const inputDate = document.getElementById('inputTaskDate').value;
-    const inputDescription = document.getElementById('inputDescribeTask').value;
+  const inputTask = document.getElementById("inputTaskTitle").value;
+  const inputDate = document.getElementById("inputTaskDate").value;
+  const inputDescription = document.getElementById("inputDescribeTask").value;
+  const isComplete = document.getElementById("inputTaskIsComplete").checked;
 
-    const generatedID = generateId();
-    const taskObject = generateTaskObject(generatedID, inputTask, inputDate, inputDescription, false);
-    incompleteCreatetaskfList.push(taskObject);
-   
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    alert('Data Berhasil disimpan, Tekan OK untuk melanjutkan');
-    saveData();
+  const generatedID = generateId();
+  const taskObject = generateTaskObject(
+    generatedID,
+    inputTask,
+    inputDate,
+    inputDescription,
+    isComplete,
+    false
+  );
+  incompleteCreatetaskfList.push(taskObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  alert("Data Berhasil disimpan, Tekan OK untuk melanjutkan");
+  saveData();
+}
+
+function changeText() {
+  const checkbox = document.getElementById("inputTaskIsComplete");
+  const textSubmit = document.getElementById("textSubmit");
+
+  if (checkbox.checked == true) {
+    textSubmit.innerText = "Daftar Task";
+  } else {
+    textSubmit.innerText = "Important Task";
+  }
+}
+
+function checkButton() {
+  const checkBox = document.querySelector("#inputTaskIsComplete");
+  checkBox.addEventListener("checked", function () {
+    if (checkBox == isComplete) addTaskToComplete(id);
+  });
 }
 
 function addTaskToCompleted(titleId) {
-    const titleTarget = findTask(titleId);
-    if (titleTarget == null) return;
-  
-    titleTarget.isCompleted = true;
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    alert('Anda telah menyelesaikan tugas, Tugas akan dialihkan pada daftar lain. Tekan OK untuk melanjutkan');
-    saveData();
+  const titleTarget = findTask(titleId);
+  if (titleTarget == null) return;
+
+  titleTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function removeTaskFromCompleted(titleId) {
-    const titleTarget = findTaskIndex(titleId);
-   
-    if (titleTarget === -1) return;
-   
-    incompleteCreatetaskfList.splice(titleTarget, 1);
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    
-    var removeTask = confirm("Apakah kamu yakin akan Menghapus Tugas?");
-        if (removeTask) {
-            window.location.pathname;
-            saveData();
-        } else{
-            alert('Gagal menghapus Tugas. Silahkan muat ulang halaman')
-        }
-}
-   
-function undoTaskFromCompleted(titleId) {
-    const titleTarget = findTask(titleId);
-   
-    if (titleTarget == null) return;
-   
-    titleTarget.isCompleted = false;
-    document.dispatchEvent(new Event(RENDER_EVENT));
+  const titleTarget = findTaskIndex(titleId);
+
+  if (titleTarget === -1) return;
+
+  incompleteCreatetaskfList.splice(titleTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+
+  var removeTask = confirm("Apakah kamu yakin akan Menghapus Tugas?");
+  if (removeTask) {
+    window.location.pathname;
     saveData();
+  } else {
+    alert("Gagal menghapus Tugas. Silahkan muat ulang halaman");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const submitForm = document.getElementById('inputTask');
-    submitForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      addTask();
-    });
+function undoTaskFromCompleted(titleId) {
+  const titleTarget = findTask(titleId);
 
-    if (isStorageExist()) {
-        loadDataFromStorage();
-    }
+  if (titleTarget == null) return;
+
+  titleTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function replaceTaskFromCompleted(titleId) {
+  const titleTarget = findTask(titleId);
+  if (titleTarget == null) return;
+
+  titleTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("inputTask");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTask();
+  });
+
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 document.addEventListener(SAVED_EVENT, () => {
-    console.log('Data berhasil di simpan.');
+  console.log("Data berhasil di simpan.");
 });
 
 document.addEventListener(RENDER_EVENT, function () {
-    const uncompletedTASKList = document.getElementById('incompleteCreatetaskfList');
-    uncompletedTASKList.innerHTML = '';
+  const uncompletedTASKList = document.getElementById(
+    "incompleteCreatetaskfList"
+  );
+  uncompletedTASKList.innerHTML = "";
 
-    const completedTASKList = document.getElementById('completeCreatetaskList');
-    completedTASKList.innerHTML = '';
-    
-    for (const taskItem of incompleteCreatetaskfList) {
-        const taskElement = makeTask(taskItem);
+  const completedTASKList = document.getElementById("completeCreatetaskList");
+  completedTASKList.innerHTML = "";
 
-        if (!taskItem.isCompleted){
-            uncompletedTASKList.append(taskElement);
-        } else{
-            completedTASKList.append(taskElement);
-        }
+  for (const taskItem of incompleteCreatetaskfList) {
+    const taskElement = makeTask(taskItem);
+
+    if (!taskItem.isCompleted) {
+      uncompletedTASKList.append(taskElement);
+    } else {
+      completedTASKList.append(taskElement);
     }
+  }
 });
