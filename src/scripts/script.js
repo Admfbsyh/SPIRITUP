cfb85c393580b5ea220d72285aec0aa335382fc2
Admfbsyh <72254331+Admfbsyh@users.noreply.github.com>
2022-12-04 09:39:25 +0700

    update script.js
    revisi script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -34,3 +34,37 @@ file.addEventListener("change", function () {
     reader.readAsDataURL(choosedFile);
   }
 });
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+const selectImage = document.querySelector(".select-image");
+const inputFile = document.querySelector("#file");
+const imgArea = document.querySelector(".img-area");
+
+selectImage.addEventListener("click", function () {
+  inputFile.click();
+});
+
+inputFile.addEventListener("change", function () {
+  const image = this.files[0];
+  if (image.size < 2000000) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const allImg = imgArea.querySelectorAll("img");
+      allImg.forEach((item) => item.remove());
+      const imgUrl = reader.result;
+      const img = document.createElement("img");
+      img.src = imgUrl;
+      imgArea.appendChild(img);
+      imgArea.classList.add("active");
+      imgArea.dataset.img = image.name;
+    };
+    reader.readAsDataURL(image);
+  }
+});
