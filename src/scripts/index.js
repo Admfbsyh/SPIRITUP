c26ce67789489d1fe3924401f8ff263e7856cb21
Indiani0 <srirahayu200207@gmail.com>
2022-12-07 12:01:31 +0700

    Add service worker configuration

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import '../styles/dashboard.css';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 const app = new App({
     button: document.querySelector('#menu'),
@@ -16,6 +17,7 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
     app.renderPage();
+    swRegister();
 });
 
 // const menu = document.getElementById('menu-label');
