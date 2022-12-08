04b80265356456d53cc4e5ec3558118f85bc50b4
Indiani0 <srirahayu200207@gmail.com>
2022-12-08 18:27:59 +0700

    Update workbox configuration & delete file cache-helper.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 
-self.addEventListener('install', (event) => {
-    console.log('Installing Service Worker ...');
+import { precacheAndRoute } from 'workbox-precaching';
+
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => {
+    console.log('Service Worker: Installed');
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Activating Service Worker ...');
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log(event.request);
-
-    event.respondWith(fetch(event.request));
+self.addEventListener('push', (event) => {
+    console.log('Service Worker: Pushed');
 });
