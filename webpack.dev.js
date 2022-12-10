38931bcb1fc5fa7651804349030a6dd3d45edb71
Candrakmb <75462751+Candrakmb@users.noreply.github.com>
2022-12-10 22:43:20 +0700

    Add files via upload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,25 @@
 const path = require('path');
-const common = require('./webpack.common');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 9000,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        open: true,
+        port: 9000,
+        client: {
+            overlay: {
+                errors: true,
+                warnings: true,
+            },
+        },
+        compress: true,
     },
-    compress: true,
-  },
 
-  plugins: [
-    new CleanWebpackPlugin()
-  ]
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
 });
